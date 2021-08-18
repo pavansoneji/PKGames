353bb19b3a563f46e2e69e8b76d27fb5b8c58d33
pkk <pavan@movingwalls.com>
2021-08-18 13:34:39 +0500

    Start

---
+++ PkkCode/Jumps2DWorld.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace PkkCode
+{
+    public class Jumps2DWorld{
+        public static void FlappyBirdJump(Rigidbody2D rigidbody2D, float jumpAmount){            
+            rigidbody2D.velocity = Vector2.up * jumpAmount;
+        }
+    }
+    
+}
---
+++ PkkCode/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PkkCode
+{
+    public class SceneLoader {
+        public static IEnumerator loadSceneLate(string sceneName, float seconds){
+            yield return new WaitForSeconds(seconds);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
+
+
